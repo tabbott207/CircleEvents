4d2b82f5bacff19b715de4fa58127608ea0b0a12
tabbott207 <tabbott207@gmail.com>
2024-11-20 13:22:49 -0500

    Event pages for 3rd party calendar events

--- home/urls.py
+++ home/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path("signup", views.signup, name="signup"),
     path("contact", views.contact, name="contact"),
     path("logout", views.logout, name="logout"),
-    path("event_detail/<str:id>", views.eventpage, name="eventpage"),
-    path("event/<str:id>/", views.event_detail, name="event_detail"),
+    path('event/<str:event_id>/', views.event_detail, name='event_detail'),
 ]
